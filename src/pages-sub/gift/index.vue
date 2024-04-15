4a90ee2adc07e92d9fd55c257e806672fd4c3929
nigo9729 <chenzhuo9729@gmail.com>
2024-04-15 20:21:51 +0800

    feat: 礼品库开发

---
+++ src/pages-sub/gift/index.vue
@@ -0,0 +1,148 @@
+<route lang="json5" type="page">
+{
+  style: { navigationBarTitleText: '礼品库' },
+}
+</route>
+
+<template>
+  <view class="wrap">
+    <view class="listitem" hover-class="none" hover-stop-propagation="false">
+      <image class="banner" :src="test" lazy-load="false"></image>
+      <view class="title" hover-class="none" hover-stop-propagation="false">
+        追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投
+        屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用
+        同步再新
+      </view>
+      <view class="type" hover-class="none" hover-stop-propagation="false">软件类</view>
+    </view>
+    <view class="listitem" hover-class="none" hover-stop-propagation="false">
+      <image class="banner" :src="test" lazy-load="false"></image>
+      <view class="title" hover-class="none" hover-stop-propagation="false">
+        追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投
+        屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用
+        同步再新
+      </view>
+      <view class="type" hover-class="none" hover-stop-propagation="false">软件类</view>
+    </view>
+    <view class="listitem" hover-class="none" hover-stop-propagation="false">
+      <image class="banner" :src="test" lazy-load="false"></image>
+      <view class="title" hover-class="none" hover-stop-propagation="false">
+        追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投
+        屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用
+        同步再新
+      </view>
+      <view class="type" hover-class="none" hover-stop-propagation="false">软件类</view>
+    </view>
+    <view class="listitem" hover-class="none" hover-stop-propagation="false">
+      <image class="banner" :src="test" lazy-load="false"></image>
+      <view class="title" hover-class="none" hover-stop-propagation="false">
+        追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投
+        屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用
+        同步再新
+      </view>
+      <view class="type" hover-class="none" hover-stop-propagation="false">软件类</view>
+    </view>
+    <view class="listitem" hover-class="none" hover-stop-propagation="false">
+      <image class="banner" :src="test" lazy-load="false"></image>
+      <view class="title" hover-class="none" hover-stop-propagation="false">
+        追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投
+        屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用
+        同步再新
+      </view>
+      <view class="type" hover-class="none" hover-stop-propagation="false">软件类</view>
+    </view>
+    <view class="listitem" hover-class="none" hover-stop-propagation="false">
+      <image class="banner" :src="test" lazy-load="false"></image>
+      <view class="title" hover-class="none" hover-stop-propagation="false">
+        追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投
+        屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用
+        同步再新
+      </view>
+      <view class="type" hover-class="none" hover-stop-propagation="false">软件类</view>
+    </view>
+    <view class="listitem" hover-class="none" hover-stop-propagation="false">
+      <image class="banner" :src="test" lazy-load="false"></image>
+      <view class="title" hover-class="none" hover-stop-propagation="false">
+        追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投
+        屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用
+        同步再新
+      </view>
+      <view class="type" hover-class="none" hover-stop-propagation="false">软件类</view>
+    </view>
+    <view class="listitem" hover-class="none" hover-stop-propagation="false">
+      <image class="banner" :src="test" lazy-load="false"></image>
+      <view class="title" hover-class="none" hover-stop-propagation="false">
+        追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投
+        屏安卓苹果影视APP永久使用 同步再新 追剧神器全网通用超清4K可投 屏安卓苹果影视APP永久使用
+        同步再新
+      </view>
+      <view class="type" hover-class="none" hover-stop-propagation="false">软件类</view>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import test from '../../static/images/photo/test.png'
+// code here
+</script>
+
+<style lang="scss" scoped>
+// here is scoped style
+.wrap {
+  height: 100vh;
+  padding: 34rpx;
+  background-color: #f4f4f4;
+}
+
+.listitem {
+  position: relative;
+  box-sizing: border-box;
+  width: 686rpx;
+  height: 220rpx;
+  padding: 24rpx;
+  margin-bottom: 32rpx;
+  background: #fff;
+  border-radius: 16rpx;
+}
+
+.banner {
+  width: 172rpx;
+  height: 172rpx;
+}
+
+.title {
+  position: absolute;
+  top: 6rpx;
+  right: 32rpx;
+  display: -webkit-box;
+  width: 434rpx;
+  height: 68rpx;
+  margin-top: 16rpx;
+  overflow: hidden;
+  font-size: 26rpx;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 34rpx;
+  color: #40434a;
+  text-align: left;
+  text-overflow: ellipsis;
+  text-transform: none;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+}
+
+.type {
+  position: absolute;
+  bottom: 24rpx;
+  left: 220rpx;
+  width: 72rpx;
+  height: 32rpx;
+  font-size: 24rpx;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 28rpx;
+  color: #b1b2b4;
+  text-align: justified;
+  text-transform: none;
+}
+</style>
--- src/pages-sub/photoBin/index.vue
+++ src/pages-sub/photoBin/index.vue
@@ -5,31 +5,56 @@
 </route>
 
 <template>
-  <view class="containerbox">
-    <!-- <wd-grid v-else :column="3">
+  <wd-tabs v-model="tab" color="#2494FF" :lineWidth="24" :line-height="3" inactiveColor="#B1B2B4">
+    <wd-tab :title="`我的`">
+      <view class="containerbox">
+        <!-- <wd-grid v-else :column="3">
       <wd-grid-item use-slot v-for="(item, index) in list" :key="index" custom-class="custom-item">
         <wd-img :key="index" :width="122" :height="122" :src="item" :enable-preview="true" />
       </wd-grid-item>
     </wd-grid> -->
-    <wd-grid :column="3">
+        <wd-grid :column="3">
+          <wd-grid-item
+            use-slot
+            v-for="(item, index) in list"
+            :key="index"
+            custom-class="custom-item"
+          >
+            <wd-img
+              :key="index"
+              :width="122"
+              :height="122"
+              :src="item"
+              custom-class="custom-pic"
+              :lazy-load="true"
+            />
+            <!-- <view class="checkbox"></view> -->
+          </wd-grid-item>
+        </wd-grid>
+      </view>
+    </wd-tab>
+    <wd-tab :title="`公共`">
+      <view class="containerbox">
+        <!-- <wd-grid v-else :column="3">
       <wd-grid-item use-slot v-for="(item, index) in list" :key="index" custom-class="custom-item">
-        <wd-img
-          :key="index"
-          :width="122"
-          :height="122"
-          :src="item"
-          custom-class="custom-pic"
-          :lazy-load="true"
-        />
-        <view class="checkbox"></view>
+        <wd-img :key="index" :width="122" :height="122" :src="item" :enable-preview="true" />
       </wd-grid-item>
-    </wd-grid>
-  </view>
+    </wd-grid> -->
+        <view class="nodata" hover-class="none" hover-stop-propagation="false">
+          <image class="nopic" :src="noPic" lazy-load="true"></image>
+          <view class="nodatatext" hover-class="none" hover-stop-propagation="false">
+            还没有上传图片
+          </view>
+          <button class="sub-btn">上传图片</button>
+        </view>
+      </view>
+    </wd-tab>
+  </wd-tabs>
 </template>
 
 <script lang="ts" setup>
 // code here
-import bjk from '@/static/images/bjk.png'
+import noPic from '../../static/images/photo/noPic.png'
 const show = ref(false)
 
 const grid = [
@@ -44,6 +69,7 @@ const grid = [
     { width: '122px', height: '122px' },
   ],
 ]
+const tab = ref<number>(1)
 const list = ref([
   'https://unpkg.com/wot-design-uni-assets/redpanda.jpg',
   'https://unpkg.com/wot-design-uni-assets/capybara.jpg',
@@ -85,7 +111,9 @@ const list = ref([
 
 <style lang="scss" scoped>
 .containerbox {
-  padding: 10px 22px;
+  min-height: 90vh;
+  padding: 10px;
+  background-color: #f4f4f4;
 }
 
 .piccontainer {
@@ -97,7 +125,7 @@ const list = ref([
 
 :deep(.custom-item) {
   height: 122px !important;
-  padding-top: 10rpx;
+  padding-top: 6rpx;
   text-align: left !important;
 }
 
@@ -111,5 +139,40 @@ const list = ref([
   height: 24px;
   border: 1px solid #000;
   border-radius: 50%;
+}
+
+.nopic {
+  width: 400rpx;
+  height: 400rpx;
+  margin-top: 150rpx;
+}
+
+.nodata {
+  text-align: center;
+}
+
+.nodatatext {
+  margin-bottom: 12rpx;
+  font-size: 28rpx;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 33rpx;
+  color: #999;
+  text-align: center;
+  text-transform: none;
+}
+
+.sub-btn {
+  width: 224rpx;
+  height: 68rpx;
+  font-size: 28rpx;
+  font-style: normal;
+  font-weight: 500;
+  color: #fff;
+  text-align: left;
+  text-align: center;
+  text-transform: none;
+  background: linear-gradient(180deg, #3aa6ff 0%, #2293ff 100%);
+  border-radius: 132rpx;
 }
 </style>
