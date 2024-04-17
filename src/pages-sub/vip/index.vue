b21015bf4f17391b382d1bb20a4e413bca62ba59
nigo9729 <chenzhuo9729@gmail.com>
2024-04-17 18:46:05 +0800

    feat: update

---
+++ src/pages-sub/vip/index.vue
@@ -0,0 +1,24 @@
+<route lang="json5" type="page">
+{
+  layout: 'default',
+  style: {
+    navigationBarTitleText: 'AI管家聊天',
+  },
+}
+</route>
+
+<template>
+  <view class="wrap"></view>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.wrap {
+  width: 750rpx;
+  height: 1992rpx;
+  padding: 32rpx;
+  background: linear-gradient(90deg, #2b2830 0%, #322f33 100%);
+  border-radius: 0rpx;
+}
+</style>
